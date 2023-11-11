3908253a4f7456d1abfb66c8e5c1da1ffc01b152
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-11-11 14:22:55 +0100

    Add challenge 189

---
+++ 0189_awaited.ts
@@ -0,0 +1,64 @@
+/*
+  189 - Awaited
+  -------
+  by Maciej Sikora (@maciejsikora) #easy #promise #built-in
+
+  ### Question
+
+  If we have a type which is wrapped type like Promise. How we can get a type which is inside the wrapped type?
+
+  For example: if we have `Promise<ExampleType>` how to get ExampleType?
+
+  ```ts
+  type ExampleType = Promise<string>
+
+  type Result = MyAwaited<ExampleType> // string
+  ```
+
+  > This question is ported from the [original article](https://dev.to/macsikora/advanced-typescript-exercises-question-1-45k4) by [@maciejsikora](https://github.com/maciejsikora)
+
+  > View on GitHub: https://tsch.js.org/189
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// My first solution
+type FirstMyAwaited<T extends Promise<unknown> |{ then: (onfulfilled: (arg: unknown) => unknown) => unknown}> = 
+  T extends Promise<Promise<infer X>>
+  ? MyAwaited<Promise<X>>
+    : T extends Promise<infer W>
+      ? W
+      : (T extends { then: (onfulfilled: (arg: infer A) => unknown) => unknown} ? A : never)
+
+// Solution after learning PromiseLike exists
+type MyAwaited<T extends PromiseLike<any>> =
+  T extends PromiseLike<infer U>
+    ? (U extends PromiseLike<any> ? MyAwaited<U> : U)
+    : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+type Z = Promise<Promise<string | number>>
+type Z1 = Promise<Promise<Promise<string | boolean>>>
+type T = { then: (onfulfilled: (arg: number) => any) => any }
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z1>, string | boolean>>,
+  Expect<Equal<MyAwaited<T>, number>>,
+]
+
+// @ts-expect-error
+type error = MyAwaited<number>
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/189/answer
+  > View solutions: https://tsch.js.org/189/solutions
+  > More Challenges: https://tsch.js.org
+*/

4d2f46f8910df492184375ee27b6b3f429b46e53
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2023-11-12 01:05:30 +0100

    feat(main): update script & add challenge 2

--- generate-file.ts
+++ generate-file.ts
@@ -20,12 +20,12 @@ const splittedUrl = url.split('/')
 if (splittedUrl.length < 2) throw new Error('Invalid URL')
 
 const puzzleNameRaw = splittedUrl.at(-2)! // We determined above that the length is at least 2
-
 const [rawChallengeId, _, ...rest] = puzzleNameRaw.split('-')
 const challengeId = rawChallengeId.replace(/^0+/, '')
 const puzzleName = rest.join('-')
 
-fetch(url)
+const getRawContentUrl = `https://raw.githubusercontent.com/type-challenges/type-challenges/main/questions/${puzzleNameRaw}/README.md`
+fetch(getRawContentUrl)
   .then(data => data.text())
   .then(async (text) => {
     let targetDirectory: Difficulty
@@ -44,7 +44,14 @@ fetch(url)
       throw new Error("Difficulty not found")
     }
 
+    if (!fs.existsSync(targetDirectory)) {
+      fs.mkdirSync(targetDirectory)
+    }
+    
     const destination = `./${targetDirectory}/${challengeId.padStart(5, "0")}_${puzzleName}.ts`
     console.log("Generating file: " + destination)
     fs.writeFileSync(destination, "")
   })
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00002-medium-return-type/README.md
+// https://raw.githubusercontent.com/type-challenges/type-challenges/main/questions/00002-medium-return-type/README.md
